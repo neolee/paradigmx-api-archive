5436bee56b06015857092dfd82be415ed4a60c10
Neo Lee <neo.lee@gmail.com>
2017-05-29 23:10:07 +0800

    Check holiday API done

--- api/project.clj
+++ api/project.clj
@@ -1,7 +1,9 @@
 (defproject api "0.1.0"
   :description "Open APIs for Paradigm X micro-services"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [metosin/compojure-api "1.2.0-alpha8"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [metosin/compojure-api "1.2.0-alpha8"]
+                 [clj-time "0.13.0"]]
   :ring {:handler api.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
--- api/src/api/handler.clj
+++ api/src/api/handler.clj
@@ -1,14 +1,49 @@
 (ns api.handler
-  (:require [compojure.api.sweet :refer :all]
+  (:require [clojure.tools.logging :as log]
+            [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]))
+            [clj-time.core :as t]
+            [clj-time.format :as f]
+            [clj-time.predicates :as pr]
+            ))
 
-(s/defschema Pizza
-  {:name s/Str
-   (s/optional-key :description) s/Str
-   :size (s/enum :L :M :S)
-   :origin {:country (s/enum :FI :PO)
-            :city s/Str}})
+(def holidays {"2017" #{"0101", "0102"
+                        "0127", "0128", "0129", "0130", "0131", "0201", "0202",
+                        "0402", "0403", "0404",
+                        "0501",
+                        "0528", "0529", "0530",
+                        "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008"}})
+
+(def rotations {"2017" #{"0122", "0204",
+                         "0401",
+                         "0527",
+                         "0930"}})
+
+(defn rotation?
+  "test given date return true if it's a rotation work day"
+  [d]
+  (let [y (subs d 0 4) md (subs d 4 8)]
+    (if (contains? (rotations y) md)
+      true
+      false))
+  )
+
+(defn weekend?
+  "test given date return true if it's a weekend day"
+  [d]
+  (pr/weekend? (f/parse (f/formatter "yyyyMMdd") d))
+  )
+
+(defn holiday?
+  "test given date return true if it's a holiday"
+  [d]
+  (let [y (subs d 0 4) md (subs d 4 8)]
+    (if (contains? (holidays y) md)
+      true
+      (if (and (weekend? d) (not (rotation? d)))
+        true
+        false)))
+  )
 
 (def app
   (api
@@ -17,7 +52,8 @@
       :spec "/swagger.json"
       :data {:info {:title "Paradigm X"
                     :description "Open APIs for Paradigm X micro-services"}
-             :tags [{:name "util", :description "Utilities"}]}}}
+             :tags [{:name "util", :description "Utilities"}
+                    {:name "holiday", :description "Holiday data services"}]}}}
 
     (context "/util" []
       :tags ["util"]
@@ -27,5 +63,15 @@
         :query-params [x :- Long, y :- Long]
         :summary "adds two numbers together"
         (ok {:result (+ x y)}))
-        )
-        ))
+      )
+
+    (context "/holiday" []
+      :tags ["holiday"]
+
+      (GET "/check" []
+        :return {:isHoliday Boolean}
+        :query-params [d :- String]
+        :summary "test given date return true if it's a holiday"
+        (ok {:isHoliday (holiday? d)}))
+      )
+    ))
