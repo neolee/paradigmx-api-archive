2dd633006cce445d8c531ed2a31b4c4961d9516f
Neo Lee <neo.lee@gmail.com>
2019-04-22 22:20:00 +0800

    Update assets

--- api/project.clj
+++ api/project.clj
@@ -1,12 +1,12 @@
 (defproject api "0.1.0"
   :description "Open APIs for Paradigm X micro-services"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/tools.logging "0.5.0-alpha.1"]
                  [clj-time "0.15.1"]
                  [metosin/compojure-api "2.0.0-SNAPSHOT"]
-                 [ring-cors "0.1.12"]]
+                 [ring-cors "0.1.13"]]
   :ring {:handler paradigmx.handler/app}
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "4.0.1"]]
-                   :plugins [[lein-ring "0.12.4"]]}}
+                   :plugins [[lein-ring "0.12.5"]]}}
   :resource-paths ["resources"])
